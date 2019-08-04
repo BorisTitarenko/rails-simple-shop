0791e330e1bd669b7f5519807b381787b10ab6e6
BorisTitarenko <specmohnat2309@gmail.com>
2019-08-04 19:37:34 +0300

    Create instrument

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,7 +1,29 @@
 class InstrumentsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
+    @instruments = Instrument.all.order("created_at desc")
   end
 
   def new
+    @instrument = current_user.instruments.build
+  end
+
+  def show
+    @instrument = Instrument.find(params[:id])
+  end
+
+  def create
+    @instrument = current_user.instruments.new(instrument_params)
+    if @instrument.save
+      redirect_to root_path
+    else
+      redirect_to new_instruments_path
+    end
+  end
+
+  private
+  def instrument_params
+    p = params.require(:instrument).permit(:image, :brand, :model, :description, :condition, :finish, :price)
   end
 end
--- app/models/instrument.rb
+++ app/models/instrument.rb
@@ -1,4 +1,14 @@
 class Instrument < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, optional: true
   has_one_attached :image
+
+  validates :brand, :model, :condition, :price, presence: true
+  validates :description, length: {maximum: 500, too_long: "Too long!"}
+  validates :price, numericality: {only_integer: true}, length: {maximum: 7}
+
+  BRAND = ["Fender", "Gibson", "Jackson", "Taylor", "Yamaha"]
+  FINISH = ["Black", "White", "Yelow", "Red", "Navi", "Clear", "Satin"]
+  CONDITION = ["New", "Excelent", "Used", "Poor"]
+
+
 end
