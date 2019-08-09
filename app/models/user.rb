e2b7b7f969d157fa0dc39b455fa7fd9e97d372ad
BorisTitarenko <specmohnat2309@gmail.com>
2019-08-09 20:54:24 +0300

    Fix bugs in cart

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
 
   has_many :instruments
-  has_one :order
+  has_many :orders
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
